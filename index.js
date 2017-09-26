3c2644a02412e7ab5f90dfa24c0b5b058d92abd2
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-09-26 16:46:23 +0100

    Support multiple operators

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const readline = require('readline-sync');
 
-console.log('Welcome to the calculator!');
+console.log('\nWelcome to the calculator!');
 console.log('==============================');
+
+console.log('\nPlease enter the operator:');
+const operator = readline.prompt();
 
 console.log('\nPlease enter the first number:');
 const firstArgument = readline.prompt();
@@ -11,5 +14,15 @@ console.log('\nPlease enter the second number:');
 const secondArgument = readline.prompt();
 const secondNumber = +secondArgument;
 
-const answer = firstNumber * secondNumber;
+let answer = 0;
+if (operator === '+') {
+    answer = firstNumber + secondNumber;
+} else if (operator === '-') {
+    answer = firstNumber - secondNumber;
+} else if (operator === '*') {
+    answer = firstNumber * secondNumber;
+} else if (operator === '/') {
+    answer = firstNumber / secondNumber;
+}
+
 console.log('\nThe answer is ' + answer);
